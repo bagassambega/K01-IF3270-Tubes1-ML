7d9448d3cfb2222af408cdad2761cf3486f71073
bagassambega <bagassambega@gmail.com>
2025-03-15 15:43:37 +0700

    fix: fix FFNN for dataset

--- src/NeuralNetwork/FFNN.py
+++ src/NeuralNetwork/FFNN.py
@@ -1,74 +1,119 @@
-from typing import List, Callable, Optional
+from typing import List, Callable
 import numpy as np
-from sklearn.datasets import fetch_openml
-
+from PerceptronException import PerceptronException
 
 class FFNN:
     """
-    Implementation of Feed Forward Neural Network
+    Implementation of Feed Forward Neural Netrowk
     """
-    def __init__(
-        self,
-        num_layers: int,
-        num_perceptrons: List[int],
-        activation_function: List[Callable],
-        loss_function: Callable,
-        dataset: str,
-        weights: List[np.ndarray[np.dtype[float], np.dtype[float]]],
-    ):
+    def __init__(self, x, y, layers: List[int], activations: List[Callable], loss_function: Callable = "mse", batch_size: int = 1, epochs: int = 10 ):
         """
-        Initialization of FFNN struct
+        Create instance of Feed Forward Neural Network
 
         Args:
-            num_layers (int): number of layers (including the input and output layer)
-            num_perceptrons (List[int]): list of number of perceptrons in each layer (not including the input layer)
-            activation_function (List[Callable[[float], float]]): list of activation function in each layer (not including the input layer)
-            dataset (str): name of dataset
-            loss_function (Callable): loss function to calculate the error in the end of forward propagation
-            weights (List[np.ndarray[np.dtype[float], np.dtype[float]]]): list of weight and bias for each layer (for example there's total 5 layer including input and output layer, there should be 4 np.ndarray of weights)
+            x (np.ndarray/np.array): dataset
+            y (np.array/np.ndarray): target
+            layers (List[int]): list of number of layers (hidden layers and output layer)
+            activations (List[Callable]): activation function for each hidden layers and output layer
+            loss_function (Callable, optional): Loss function. Defaults to "mse".
+            batch_size (int, optional): batch size. Defaults to 1.
+            epochs (int, optional): number of epochs. Defaults to 10.
+
+        Raises:
+            PerceptronException: Check if length for array activations and layers are the same
         """
-        # Load the dataset
-        self.X = None
-        self.y = None
-        self.load_dataset(dataset=dataset)
+        self.x = x # Input of layer 0 (input layer)
+        self.y = y
+        self.layers = layers
 
-        # Check for layers and number of perceptrons
-        assert len(num_perceptrons) == num_layers - 1 # Input layer not counted
-        assert len(activation_function) == num_layers - 1 # Input layer not counted
-        self.num_layers: int = num_layers
-        self.num_perceptrons: List[int] = num_perceptrons
-        self.activation_function: List[Callable] = activation_function
-        self.loss = loss_function
+        self.activations = activations
+        if len(activations) != len(layers):
+            raise PerceptronException("Number of activations must be the same with number of hidden layers + 1 output layer")
 
-        # Check weights
-        assert len(weights) == num_layers - 1
-        self.weights = weights
-        for i in range(num_layers):
-            assert len(self.weights[i]) == (num_perceptrons[i] + 1) # Check for bias as w0 too, so num_perceptrons in each layer + 1
-
-        for i in range(len(self.X)):
-            assert len(self.weights[i]) == len(self.X[i]) + 1 # Check if number of features is match with number of weight + bias
+        self.weights = [[] for _ in range(len(self.layers))]
+        self.loss_function = loss_function
+        self.epochs = epochs
+        self.batch_size = batch_size
 
 
-    def train(
-        self,
-        X_train,
-        y_train,
-        X_val,
-        y_val,
-        batch_size: Optional[int],
-        learning_rate: Optional[float],
-        epochs: Optional[int] = 1,
-        verbose: Optional[int] = 0,
-    ):
+    def initialize_weights(self, method: str = "random"): 
+        """
+        Initialize weights
+
+        Args:
+            method (str, optional): weights initialization. Defaults to "random".
+        """
+        # TODO: implement initialize weight
+        self.weights = [[] for _ in range(len(self.layers))]
+        for i, _ in enumerate(self.layers):
+            # Banyak weight antara layer (n-1) ke layer n = ((n-1)th layer + 1 (bias)) * layer n
+            self.weights[i] = np.random.rand(self.x.shape[1] + 1, self.layers[i]) # + 1 for bias, bias is the first row
+
+
+    def net(self, inputs, weight):
+        """
+        Calculate net of layer n
+
+        Args:
+            inputs (array of input): inputs
+            weight (matrix of weight, size of ((n-1)-th layer + 1) * n-th layer): matrix of weights. Bias included as w0
+
+        Returns:
+            _type_: _description_
+        """
+        return weight[0] + np.dot(inputs, weight[1:])
+
+    def activate(self, activation: Callable, val):
+        """
+        Activation function
+
+        Args:
+            activation (Callable): activation method function (function)
+            val (_type_): value
+
+        Returns:
+            _type_: _description_
+        """
+        return activation(val)
+
+    def forward(self):
         pass
 
+    def backward(self):
+        pass
 
-    def load_dataset(self, dataset: str):
-        """
-        Load dataset into X (matrix of instances, features) and y (list of target)
+    def train(self):
+        # Forward propagation
+        # TODO: implement mini-batch (multiple row of dataset) and epoch
+        # Layer input merupakan satu row, dan di NN layer input berupa x1 x2 dst merupakan fitur-fiturnya
+        for _ in range(self.epochs):
+            for _ in range(0, self.x.shape[0], self.batch_size):
+                self.forward()
+                self.backward()
 
-        Args:
-            dataset (str): name of dataset to OpenML
-        """
-        self.X, self.y = fetch_openml(name=dataset, version=1, return_X_y=True, as_frame=False, cache=True)
+# Contoh
+def relu(val):
+    return np.maximum(0, val)
+
+# x(2), h1(2), o(1)
+if __name__ == "__main__":
+    x = np.array([[0.05, 0.1],
+        [0.8, 0.7]])
+    y = np.array([0, 1])
+
+    ffnn = FFNN(x, y, [2, 1], [relu, relu])
+    ffnn.initialize_weights()
+
+    # misalkan untuk data pertama dulu (batch 1 jika batch_size = 1)
+
+    # h1
+    net_h1 = ffnn.net(x[0], ffnn.weights[0])
+    print(f"Net h1: {net_h1}")
+    h1 = ffnn.activate(relu, net_h1)
+    print(f"H1: {h1}")
+
+    # o
+    net_o = ffnn.net(h1, ffnn.weights[1])
+    print(f"Net O: {net_o}")
+    o = ffnn.activate(relu, net_o)
+    print(f"O: {o}")
