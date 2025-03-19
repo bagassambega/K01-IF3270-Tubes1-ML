1255c66ba9fb128db41a0c4d7c310eb657f58990
HenryofSkalitz1202 <edarrell1202@gmail.com>
2025-03-19 16:38:58 +0700

    feat: integrate with struct, decompose bias into separate matrix

--- src/NeuralNetwork/FFNN.py
+++ src/NeuralNetwork/FFNN.py
@@ -1,6 +1,7 @@
 from typing import List, Callable
 import numpy as np
 from PerceptronException import PerceptronException
+from WeightGenerator import zeroInitialization, random_uniform_distribution, normal_distribution
 
 class FFNN:
     """
@@ -31,6 +32,7 @@ class FFNN:
             raise PerceptronException("Number of activations must be the same with number of hidden layers + 1 output layer")
 
         self.weights = [[] for _ in range(len(self.layers))]
+        self.bias = [[] for _ in range(len(self.layers))]
         self.loss_function = loss_function
         self.epochs = epochs
         self.batch_size = batch_size
@@ -43,12 +45,68 @@ class FFNN:
         Args:
             method (str, optional): weights initialization. Defaults to "random".
         """
-        # TODO: implement initialize weight
-        self.weights = [[] for _ in range(len(self.layers))]
-        for i, _ in enumerate(self.layers):
-            # Banyak weight antara layer (n-1) ke layer n = ((n-1)th layer + 1 (bias)) * layer n
-            self.weights[i] = np.random.rand(self.x.shape[1] + 1, self.layers[i]) # + 1 for bias, bias is the first row
 
+        if (method == "zero"):
+            for i, _ in enumerate(self.layers):
+                self.weights[i], self.bias[i] = zeroInitialization(rowDim=self.x.shape[1], colDim=self.layers[i])
+
+
+        elif(method == "normal"):
+            while True:
+                mean = input("Mean: ")
+                try:
+                    mean = float(mean)
+                    break
+                except ValueError:
+                    print("Mean must be a valid number")
+            
+            while True:
+                variance = input("Variance: ")
+                try:
+                    variance = float(variance)
+                    break
+                except ValueError:
+                    print("Variance must be a valid number")
+
+            seed = input("Seed: ")
+            if not(seed.isdigit()):
+                seed = None
+
+            for i, _ in enumerate(self.layers):
+                if i == 0:
+                    self.weights[i], self.bias[i] = normal_distribution(mean=mean, variance=variance, rowDim=self.x.shape[1], colDim=self.layers[i], seed=seed)
+                else:
+                    self.weights[i], self.bias[i] = normal_distribution(mean=mean, variance=variance, rowDim=self.layers[i - 1], colDim=self.layers[i], seed=seed)
+
+        else:
+            while True:
+                lower_bound = input("Lower bound: ")
+                try:
+                    lower_bound = float(lower_bound)
+                    break
+                except ValueError:
+                    print("Lower bound must be a valid number")
+            
+            while True:
+                upper_bound = input("Upper bound: ")
+                try:
+                    variance = float(variance)
+                    if(upper_bound > lower_bound):
+                        break
+                    else:
+                        print("Upper bound must be greater than lower bound")
+                except ValueError:
+                    print("Variance must be a valid number")
+
+            seed = input("Seed: ")
+            if not(seed.isdigit()):
+                seed = None
+
+            for i, _ in enumerate(self.layers):
+                if i == 0:
+                    self.weights[i], self.bias[i] = random_uniform_distribution(lower_bound=lower_bound, upper_bound=upper_bound, rowDim=self.x.shape[1], colDim=self.layers[i], seed=seed)
+                else:
+                    self.weights[i], self.bias[i] = random_uniform_distribution(lower_bound=lower_bound, upper_bound=upper_bound, rowDim=self.layers[i - 1], colDim=self.layers[i], seed=seed)
 
     def net(self, inputs, weight):
         """
--- src/NeuralNetwork/WeightGenerator.py
+++ src/NeuralNetwork/WeightGenerator.py
@@ -1,10 +1,13 @@
 import random
 import math
 
+#rowDim = jumlah input node (di layer n-1, ga termasuk bias)
+#colDim = jumlah output node (di layer n)
 def zeroInitialization(rowDim, colDim):
     weightMatrix = [[0 for _ in range(colDim)] for _ in range(rowDim)]
+    biasMatrix = [[0 for _ in range(1)] for _ in range(rowDim)]
 
-    return weightMatrix
+    return weightMatrix, biasMatrix
 
 def random_uniform_distribution(lower_bound, upper_bound, rowDim, colDim, seed=None):
     weightMatrix = zeroInitialization(rowDim, colDim)
@@ -15,8 +18,12 @@ def random_uniform_distribution(lower_bound, upper_bound, rowDim, colDim, seed=N
     for i in range(rowDim):
         for j in range(colDim):
             weightMatrix[i][j] = round(random.uniform(lower_bound, upper_bound), 4)
+    
+    #generate bias
+    bias = round(random.uniform(lower_bound, upper_bound), 4)
+    biasMatrix = [[bias for _ in range(1)] for _ in range(colDim)]
             
-    return weightMatrix
+    return weightMatrix, biasMatrix
 
 def normal_distribution(mean, variance, rowDim, colDim, seed=None):
     weightMatrix = zeroInitialization(rowDim, colDim)
@@ -28,5 +35,10 @@ def normal_distribution(mean, variance, rowDim, colDim, seed=None):
         for j in range(colDim):
             weightMatrix[i][j] = round(random.normalvariate(mean, math.sqrt(variance)), 4)
 
-    return weightMatrix
+    #generate bias
+    bias = round(random.normalvariate(mean, math.sqrt(variance)), 4)
+    biasMatrix = [[bias for _ in range(1)] for _ in range(colDim)]
+
+
+    return weightMatrix, biasMatrix
 
