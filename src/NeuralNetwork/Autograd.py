a303a7092321573534eec7f5307e062a24fc994a
bagassambega <bagassambega@gmail.com>
2025-03-25 13:40:25 +0700

    fix: add activation function in scalar

--- src/NeuralNetwork/Autograd.py
+++ src/NeuralNetwork/Autograd.py
@@ -22,6 +22,7 @@ class Scalar:
         self._parents = set(parents)  # Menyimpan node asal
         self._operation = operation  # Menyimpan operasi yang dilakukan parents
 
+    #! Basic getter/setter
     def get_value(self) -> int | float:
         """
         Getter untuk value
@@ -46,7 +47,26 @@ class Scalar:
         """
         self._backward = _backward
 
-    def __add__(self, other):
+    def get_parents(self):
+        """
+        Return parents of the Scalar value
+
+        Returns:
+            parents: parents of the Scalar
+        """
+        return self._parents
+
+    def get_operation(self) -> str:
+        """
+        Return the operation that creating this Scalar value. '' if not exists
+
+        Returns:
+            str: Operation creating this scalar
+        """
+        return self._operation
+
+    #! Basic math operator
+    def __add__(self, other) -> 'Scalar':
         """ Operator penjumlahan antara dua objek Scalar. Dipanggil otomatis di self + other"""
         other = other if isinstance(other, Scalar) else Scalar(other)
         result = Scalar(self.value + other.value, (self, other), '+')
@@ -58,7 +78,7 @@ class Scalar:
 
         return result
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> 'Scalar':
         """ Operator perkalian antara dua objek Scalar. Dipanggil otomatis di self * other"""
         other = other if isinstance(other, Scalar) else Scalar(other)
         result = Scalar(self.value * other.value, (self, other), '*')
@@ -70,9 +90,10 @@ class Scalar:
 
         return result
 
-    def __pow__(self, exponent):
+    def __pow__(self, exponent) -> 'Scalar':
         """ Operator pemangkatan. Dipanggil otomatis di self ** other"""
-        assert isinstance(exponent, (float | int, float | int)), "Pangkat harus berupa angka."
+        exponent = exponent.value if isinstance(exponent, Scalar) else exponent
+        assert isinstance(exponent, (float, int)), "Nilai harus bilangan"
         result = Scalar(self.value ** exponent, (self,), f'**{exponent}')
 
         def _backward():
@@ -82,31 +103,31 @@ class Scalar:
 
         return result
 
-    def __neg__(self):
+    def __neg__(self) -> 'Scalar':
         """ Operator negasi (-self). """
         return self * -1
 
-    def __sub__(self, other):
+    def __sub__(self, other) -> 'Scalar':
         """ Operator pengurangan (self - other). """
         return self + (-other)
 
-    def __rsub__(self, other):
+    def __rsub__(self, other) -> 'Scalar':
         """ Operator pengurangan untuk other - self. """
         return other + (-self)
 
-    def __radd__(self, other):
+    def __radd__(self, other) -> 'Scalar':
         """ Operator penjumlahan untuk other + self. """
         return self + other
 
-    def __rmul__(self, other):
+    def __rmul__(self, other) -> 'Scalar':
         """ Operator perkalian untuk other * self. """
         return self * other
 
-    def __truediv__(self, other):
+    def __truediv__(self, other) -> 'Scalar':
         """ Operator pembagian (self / other). """
         return self * (other ** -1)
 
-    def __rtruediv__(self, other):
+    def __rtruediv__(self, other) -> 'Scalar':
         """ Operator pembagian untuk other / self. """
         return other * (self ** -1)
 
@@ -114,8 +135,47 @@ class Scalar:
         """ Representasi string dari objek Scalar. """
         return f"Scalar(value={self.value}, grad={self.grad})"
 
-    def relu(self) -> float | int:
-        """ Fungsi aktivasi ReLU (Rectified Linear Unit). Dikalikan dengan nilai out.grad karena chain rule (dz/dx = dz/dy * dy/dx)
+    def log(self) -> 'Scalar':
+        """ Compute natural logarithm of the Scalar """
+        assert self.value > 0, "Log input must be positive"
+        out = Scalar(math.log(self.value), (self,), 'log')
+
+        def _backward():
+            self.grad += (1 / self.value) * out.grad
+        out.set_backward(_backward)
+
+        return out
+
+    def __mod__(self, other: 'Scalar') -> 'Scalar':
+        """
+        Operasi modulo
+
+        Args:
+            other (Scalar): nilai lain yang akan dimodulo
+
+        Returns:
+            Scalar
+        """
+        return Scalar(self.value % other.value)
+
+
+    def __rmod__(self, other: 'Scalar') -> 'Scalar':
+        """
+        Operasi modulo
+
+        Args:
+            other (Scalar): nilai lain yang akan dimodulo
+
+        Returns:
+            Scalar
+        """
+        return Scalar(other.value % self.value)
+
+
+    #! Activation function
+    def relu(self) -> 'Scalar':
+        """ Fungsi aktivasi ReLU (Rectified Linear Unit). Dikalikan dengan nilai out.grad
+          karena chain rule (dz/dx = dz/dy * dy/dx)
         f(x) = max(0, x). Turunan: if x > 0 return 1, else return 0
         """
         result = Scalar(0 if self.value <= 0 else self.value, (self,), 'ReLU')
@@ -127,7 +187,7 @@ class Scalar:
 
         return result
 
-    def sigmoid(self) -> float | int:
+    def sigmoid(self) -> 'Scalar':
         """
         Fungsi aktivasi sigmoid: f(x) = 1/(1 + e^(-x))
         Turunan: o * (1 - o)
@@ -140,12 +200,12 @@ class Scalar:
 
         return out
 
-    def linear(self) -> float | int:
+    def linear(self) -> 'Scalar':
         """
         Fungsi aktivasi linear
 
         Returns:
-            float/int: Scalar value hasil linea
+            float/int/Scalar: Scalar value hasil linear
         """
         out = Scalar(self.value, (self,), 'linear')
 
@@ -155,13 +215,13 @@ class Scalar:
 
         return out
 
-    def tanh(self) -> int | float:
+    def tanh(self) -> 'Scalar':
         """
         Fungsi aktivasi tanh: f(x) = tanh(x)
         Turunan: 1 - tanh(x) ** 2
 
         Returns:
-            int | float: _description_
+            int | float: Scalar value hasil tanh
         """
         out = Scalar(math.tanh(self.value), (self,), 'tanh')
 
@@ -176,13 +236,13 @@ class Scalar:
         Melakukan propagasi mundur untuk menghitung gradien dari nilai ini terhadap
         semua nilai dalam computational graph yang berkontribusi terhadapnya.
         """
-        topo_order = []
+        topo_order: list[Scalar] = []
         visited = set()
 
-        def build_topology(node):
+        def build_topology(node: Scalar):
             if node not in visited:
                 visited.add(node)
-                for parent in node._parents:
+                for parent in node.get_parents():
                     build_topology(parent)
                 topo_order.append(node)
 
@@ -190,4 +250,4 @@ class Scalar:
         self.grad = 1
 
         for node in reversed(topo_order):
-            node._backward()
+            node.get_backward()
